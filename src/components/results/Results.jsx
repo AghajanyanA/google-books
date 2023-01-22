97840050606e21f60e14dbd81ba3abe793b08120
AghajanyanA <astara@null.net>
2023-01-22 20:04:09 +0400

    separate component for cards

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import c from './results.module.css'
 import Preloader from '../../assets/Preloader/Preloader'
-import { Link } from 'react-router-dom'
+import Card from './Card/Card'
 
 const Results = (props) => {
 
@@ -20,18 +20,7 @@ const Results = (props) => {
                 <div>
                     <h3 className={c.resultsCount}>Results Found: {props.totalItems}</h3>
                     <div className={c.cards}>
-                        {props.results?.map(data => <div key={data.id} className={c.card} >
-                            <Link to={`/${data.id}`}>
-                                <img src={data.volumeInfo?.imageLinks?.thumbnail} alt="sorry" className={c.image} />
-                            </Link>
-                            <p className={c.title} >
-                                <Link to={`/${data.id}`}> {data.volumeInfo.title} </Link>
-                            </p>
-                            <div className={c.cardBottomWrap} >
-                                {data.volumeInfo.authors ? <div className={c.authors} > {data.volumeInfo.authors.join(', ')} </div> : ''}
-                                <span className={c.categories} > {data.volumeInfo.categories} </span>
-                            </div>
-                        </div>)}
+                        {props.results?.map(data => <Card key={data.id} data={data} /> )}
                     </div>
                     {props.isLoading ? '' : <button className={c.loadMoreBTN} onClick={loadMore} disabled={props.noMoreResults} >{props.noMoreResults ? 'No more results found' : 'Load More'} </button> }
                 </div>}
